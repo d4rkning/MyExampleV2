91665b5a536ed908e4459274aeaf37ebea6217d4
chriss.schellekens12@gmail.com <d4rkning>
2026-05-14 08:21:39 +0200

    Fixed MyRange linear interpolation implementation

--- src/myrange.jl
+++ src/myrange.jl
@@ -14,6 +14,4 @@ function _MyRange(a::MyRange, i::Int)
     return b
 end
 Base.getindex(a::MyRange, i::Int) = _MyRange(a, i)
-
-
-(c::MyRange)(b::Float64) = c.start + c.step * (b-1)
+Base.getindex(a::MyRange, b::Float64) = a.start + a.step * (b-1)
