f2317d6ccf6a12855df26274dba2d9e9fc10176f
chriss.schellekens12@gmail.com <d4rkning>
2026-05-15 21:41:06 +0200

    more metaprogramming explainations

--- src/metaprogramming.jl
+++ src/metaprogramming.jl
@@ -15,3 +15,32 @@ macro myevalpoly(x, p...)
     end
     return ex
 end
+
+prog = "1 + 1"
+ex1 = Meta.parse(prog)
+typeof(ex1)
+ex1.head
+ex1.args
+ex2 = Expr(:call, :+, 1, 1)
+dump(ex2)
+ex3 = Meta.parse("(4 + 4) / 2")
+Meta.show_sexpr(ex3)
+
+s = :foo
+typeof(s)
+:foo === Symbol("foo")
+
+Symbol("1foo")
+Symbol("func", 10)
+Symbol(:var, '-', "sym")
+ex = :(a+b*c+1)
+typepof(ex)
+ex.head
+ex.args
+dump(ex)
+Meta.@dump(ex)
+ex = quote
+    x = 1
+    y = 2
+    x + y
+end
