a2c98b083425749f92aafe592a4a279e5e255f05
chriss.schellekens12@gmail.com <d4rkning>
2026-05-15 10:36:45 +0200

    Added  more operators for MultiVariableDual

--- src/dualnumbers.jl
+++ src/dualnumbers.jl
@@ -24,7 +24,26 @@ end
 Base.:+(a::MultiVariateDual, b::MultiVariateDual) = MultiVariateDual(a.x + b.x, a.dx + b.dx)
 Base.:*(a::MultiVariateDual, b::MultiVariateDual) = MultiVariateDual(a.x * b.x, b.dx * a.x + b.x * a.dx)
 
+Base.:+(a::Float64, b::MultiVariateDual) = MultiVariateDual(a + b.x, b.dx)
+Base.:+(a::MultiVariateDual, b::Float64) = b + a
+
+Base.:*(a::Float64, b::MultiVariateDual) = MultiVariateDual(a * b.x, a .* b.dx)
+Base.:*(a::MultiVariateDual, b::Float64) = b * a
+
+Base.:-(a::Float64, b::MultiVariateDual) = MultiVariateDual(a - b.x, -b.dx)
+Base.:-(a::MultiVariateDual, b::Float64) = MultiVariateDual(a.x - b, a.dx)
+
+Base.:/(a::MultiVariateDual, b::MultiVariateDual) = MultiVariateDual(a.x / b.x, (b.x .* a.dx .- a.x .* b.dx) ./ (b.x^2))
+Base.:/(a::MultiVariateDual, b::Real) = MultiVariateDual(a.x / b, a.dx ./ b)
+
+Base.:^(a::MultiVariateDual, p::Real) = MultiVariateDual(a.x^p, (p * a.x^(p-1)) .* a.dx)
+
+Base.exp(a::MultiVariateDual) = MultiVariateDual(exp(a.x), exp(a.x) .* a.dx)
+
+Base.log(a::MultiVariateDual) = MultiVariateDual(log(a.x), a.dx ./ a.x)
+
 h(x::MultiVariateDual, y::MultiVariateDual) = x+y
+
 h(MultiVariateDual(2.0, [1.0, 0.0]), MultiVariateDual(3.0, [0.0, 1.0]))
 
 i(x::MultiVariateDual, y::MultiVariateDual) = x*y
