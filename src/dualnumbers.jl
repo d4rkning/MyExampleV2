f6b52c5e718886517ebb831d471a0d8b02103129
chriss.schellekens12@gmail.com <d4rkning>
2026-05-15 09:51:57 +0200

    First implementation of dual numbers

--- src/dualnumbers.jl
+++ src/dualnumbers.jl
@@ -2,3 +2,16 @@ struct Dual
     x::Float64
     dx::Float64
 end
+
+Base.:+(a::Dual, b::Dual) = Dual(a.x + b.x, a.dx + b.dx)
+Base.:*(a::Dual, b::Dual) = Dual(a.x * b.x, b.dx * a.x + b.x * a.dx)
+Base.:-(a::Dual, b::Dual) = Dual(a.x - b.x, a.dx - b.dx)
+Base.:/(a::Dual, b::Dual) = Dual(a.x / b.x, (a.dx * b.x - a.x * b.dx) / b.x^2)
+Base.sin(a::Dual) = Dual(sin(a.x), cos(a.x) * a.dx)
+Base.cos(a::Dual) = Dual(cos(a.x), -sin(a.x) * a.dx)    
+
+
+f(x::Dual) = sin(x) + x*x
+f(Dual(2, 1))
+g(x::Dual, y::Dual) = x*y
+g(Dual(2, 1), Dual(3, 1))
